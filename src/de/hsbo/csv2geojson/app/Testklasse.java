2646be0c4f5bc07f4d3c2b818457b16f3441d351
Christian Koert <christian.koert@hs-bochum.de>
2018-11-05 08:30:42 +0100

    Using unlimited number of fields

--- src/de/hsbo/csv2geojson/app/Testklasse.java
+++ src/de/hsbo/csv2geojson/app/Testklasse.java
@@ -5,9 +5,11 @@ import java.util.ArrayList;
 
 
 import de.hsbo.csv2geojson.converter.CsvObject;
+import de.hsbo.csv2geojson.converter.GeoJsonFile;
 import de.hsbo.csv2geojson.converter.GeoJsonObject;
 import de.hsbo.csv2geojson.converter.HtmlObject;
 import de.hsbo.csv2geojson.converter.PropertiesObject;
+import de.hsbo.csv2geojson.geometry.Point;
 import de.hsbo.csv2geojson.geometry.Stadion;
 
 
@@ -15,27 +17,28 @@ public class Testklasse{
 
 	public static void main(String[] args) throws Exception{
 		
-		String dateipfad = "E:\\Google Drive\\Arbeit\\Projekt_CSV2GEOJSON\\workspace-wacodis\\CSV2GEOJSON\\stadien.properties";
-		//"C:\\Users\\chris\\OneDrive for Business\\SHK_Stelle\\CSV2GEOJSON\\stadien.properties"
+		String dateipfad =args[0];
+		//"E:\\Google Drive\\Arbeit\\Projekt_CSV2GEOJSON\\workspace-wacodis\\CSV2GEOJSON\\stadien.properties"
+		//"C:\\Users\chris\\OneDrive for Business\\SHK_Stelle\\workspace_WaCoDiS\\CSV2GEOJSON\\stadien.properties"
 		PropertiesObject prop = new PropertiesObject(dateipfad);
 		HtmlObject html = new HtmlObject(prop.getPropUrl());
 		
 		BufferedReader kurowski = html.readWebsite();
 		CsvObject csv = new CsvObject(kurowski, prop.getPropSep(), prop.getPropXField(), prop.getPropYField());
 		
-		ArrayList<Stadion> blindert = csv.readCSV();
-		GeoJsonObject geoJson = new GeoJsonObject(blindert);
+		ArrayList<Point> blindert = csv.readCSV(prop.getSepFields());
+		GeoJsonObject geoJson = new GeoJsonObject(blindert, prop.getPropXField(), prop.getPropYField());
 		
 		String geoJsonString = geoJson.createGEOJSON();
-		/*
-		GeoJsonFile file = new GeoJsonFile(geoJsonString, prop.getPropZiel());
+		
+		GeoJsonFile file = new GeoJsonFile(geoJsonString, prop.getPropDest());
 		file.createFile();
-		*/
+		
 		System.out.println();
 		System.out.println(prop.getPropDest());
 		// System.out.println(prop.getPropUrl());
-		// System.out.println(geoJsonString);
-		System.out.println(prop.getSepFields());
+		//System.out.println(geoJsonString);
+		//System.out.println(prop.getSepFields());
 	}
 
 }
