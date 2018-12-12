8d9268d765437827f8ff6bcbddb37ef441b1bf36
Christian Koert <christian.koert@hs-bochum.de>
2018-12-12 14:59:06 +0100

    Add exception handling in main class

--- src/main/java/de/hsbo/csv2geojson/app/Application.java
+++ src/main/java/de/hsbo/csv2geojson/app/Application.java
@@ -1,23 +1,29 @@
 package de.hsbo.csv2geojson.app;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 
+import org.apache.http.client.ClientProtocolException;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 import de.hsbo.csv2geojson.converter.CsvReader;
 import de.hsbo.csv2geojson.converter.GeoJsonFileWriter;
 import de.hsbo.csv2geojson.converter.GeoJsonConverter;
 import de.hsbo.csv2geojson.converter.HtmlReader;
 import de.hsbo.csv2geojson.converter.PropertiesReader;
+import de.hsbo.csv2geojson.exception.WrongFieldSeparatorException;
 import de.hsbo.csv2geojson.geometry.CsvPoint;
 
 
 public class Application{
 
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args){
 		// long start = System.currentTimeMillis();
 		String path =args[0];
 		PropertiesReader prop = new PropertiesReader();
+		try {
 		prop.readProperties(path);
 		HtmlReader html = new HtmlReader(prop.getPropUrl());
 		
@@ -30,13 +36,23 @@ public class Application{
 		String geoJsonString = geoJson.createGEOJSON();
 		
 		GeoJsonFileWriter file = new GeoJsonFileWriter(geoJsonString, prop.getPropTarget());
+				
 		file.createFile();
-		// long end = System.currentTimeMillis();
+		}
+		catch(ClientProtocolException e) {
+			e.printStackTrace();
+		}
+		catch(JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		catch(WrongFieldSeparatorException e) {
+			e.printStackTrace();
+		}
+		catch(IOException e) {
+			e.printStackTrace();
+		}
 		System.out.println("created geojson-file");
-		// System.out.println(end-start);
-		// System.out.println(prop.getPropUrl());
-		//System.out.println(geoJsonString);
-		//System.out.println(prop.getSepFields());
+	
 	}
 
 }
